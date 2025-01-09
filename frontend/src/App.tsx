90dc094ab561e2d27c7b8dab539ea5763154f3d6
sauravdhoju <sauravdhoju12@gmail.com>
2025-01-09 16:17:58 +0545

    app name updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Button, Typography } from "@mui/material";
+import theme from './theme'
+import { ThemeProvider } from "@mui/material/styles";
 
 const App: React.FC = () => {
     return (
-        <div style={{ textAlign: "center", marginTop: "20px" }}>
-            <Typography variant="h3" component="h1" gutterBottom>
-                Welcome to MUI with React and TypeScript
-            </Typography>
-            <Button variant="contained" color="primary">
-                Click Me
-            </Button>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Typography variant="h1">Hello World</Typography>
+            <Button variant="contained">Click me</Button>
+        </ThemeProvider>
     );
 };
 
