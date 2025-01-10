3cc2705516a75a56a385869a21c2d1da0b57dd5f
Sujalcha7 <sujalkoju97@gmail.com>
2025-01-10 18:18:03 +0545

    get doctor by id fixed

--- backend/src/router/user-router.ts
+++ backend/src/router/user-router.ts
@@ -1,20 +1,28 @@
 // routes/user-router.ts
-import express from 'express';
+import express from "express";
 import {
-  deleteUser,
-  getAllUsers,
-  getCurrentUser,
-  updateUser,
-  getAllDoctors,
-  getAllPatients
-} from '../controllers/user-controllers';
-import { isAuthenticated } from '../middlewares/index';
+    deleteUser,
+    getAllUsers,
+    getCurrentUser,
+    updateUser,
+    getAllDoctors,
+    getAllPatients,
+    getDoctorById,
+    validateObjectId,
+} from "../controllers/user-controllers";
+import { isAuthenticated } from "../middlewares/index";
 
 export default (router: express.Router) => {
-  router.get('/api/user', isAuthenticated, getCurrentUser);
-  router.get('/api/users', isAuthenticated, getAllUsers); 
-  router.get('/api/doctors', isAuthenticated, getAllDoctors);
-  router.get('/api/patients', isAuthenticated, getAllPatients);
-  router.put('/api/user/update', isAuthenticated, updateUser);
-  router.delete('/api/user/delete', isAuthenticated, deleteUser);
+    router.get("/api/user", isAuthenticated, getCurrentUser);
+    router.get("/api/users", isAuthenticated, getAllUsers);
+    router.get("/api/doctors", isAuthenticated, getAllDoctors);
+    router.get("/api/patients", isAuthenticated, getAllPatients);
+    router.put("/api/user/update", isAuthenticated, updateUser);
+    router.delete("/api/user/delete", isAuthenticated, deleteUser);
+    router.get(
+        "/api/doctor/:id",
+        isAuthenticated,
+        validateObjectId,
+        getDoctorById
+    );
 };
