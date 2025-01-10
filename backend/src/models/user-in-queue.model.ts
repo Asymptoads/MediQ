1a54895483b476d8e7eaabf1a21559926257add6
Sujalcha7 <sujalkoju97@gmail.com>
2025-01-10 17:10:14 +0545

    backend update and delete acc for both user and doctor

--- backend/src/models/user-in-queue.model.ts
+++ backend/src/models/user-in-queue.model.ts
@@ -1,51 +1,51 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
 const userInQueueSchema = new Schema({
-  registered_at: { type: Date, default: Date.now },
-  remaining_at_the_time_of_register: { type: Number },
-  // Reference to the VisitCategory model
-  category: {
-    type: String,
-    required: true,
-    enum: [
-      'Emergency Cases',
-      'Follow-up Visits',
-      'First-time Consultations',
-      'Lab Tests Only',
-      'Report Collections',
-      'Prescription Refills',
-      // 'X-ray',         // Added as example
-      // 'MRI Scan',      // Added as example
-      // 'General Checkup',  // Added as example
-      // 'Blood Test',    // Added as example
-      // Add more categories as needed
-    ],
-  },
-  description: { type: String, default: null },
+    registered_at: { type: Date, default: Date.now },
+    remaining_at_the_time_of_register: { type: Number },
+    // Reference to the VisitCategory model
+    category: {
+        type: String,
+        required: true,
+        enum: [
+            "Emergency Cases",
+            "Follow-up Visits",
+            "First-time Consultations",
+            "Lab Tests Only",
+            "Report Collections",
+            "Prescription Refills",
+            // 'X-ray',         // Added as example
+            // 'MRI Scan',      // Added as example
+            // 'General Checkup',  // Added as example
+            // 'Blood Test',    // Added as example
+            // Add more categories as needed
+        ],
+    },
+    description: { type: String, default: null },
 
-  queue_id: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: 'Queue',
-  },
+    queue_id: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "Queue",
+    },
 
-  user_id: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
+    user_id: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
 
-  doctor_id: {
-    type: Schema.Types.ObjectId, 
-    ref: 'User',  // Reference to the Doctor model
-    default: null 
-  },
-  is_suspended: { type: Boolean, default: false },
-  suspended_at: { type: Date, default: null }
-  is_completed: { type: Boolean, default: false },
-  completed_at: { type: Date, default: null }
+    doctor_id: {
+        type: Schema.Types.ObjectId,
+        ref: "User", // Reference to the Doctor model
+        default: null,
+    },
+    is_suspended: { type: Boolean, default: false },
+    suspended_at: { type: Date, default: null },
+    is_completed: { type: Boolean, default: false },
+    completed_at: { type: Date, default: null },
 });
 
 // Middleware to calculate total_waited
@@ -58,5 +58,5 @@ const userInQueueSchema = new Schema({
 //   next();
 // });
 
-const userInQueueModel = mongoose.model('userInQueue', userInQueueSchema);
-export {userInQueueModel, userInQueueSchema};
+const userInQueueModel = mongoose.model("userInQueue", userInQueueSchema);
+export { userInQueueModel, userInQueueSchema };
