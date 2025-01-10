761d02d9c351a00fd0dffd2729226612b4ad042c
Sreinumder <Rojgosai02@gmail.com>
2025-01-11 00:05:15 +0545

    created appointment controllers

--- backend/src/router/appointment-router.ts
+++ backend/src/router/appointment-router.ts
@@ -1,12 +1,21 @@
 // routes/user-router.ts
 import express from "express";
-import { createappointment } from "../controllers/appointment-controllers";
+import {
+  createAppointment,
+  getAppointments,
+  getAppointmentsByUserId,
+  markAppointmentAsSuspended,
+  // markAppointmentAsOperated,
+  markAppointmentAsCompleted,
+} from "../controllers/appointment-controllers";
 
 import { isAuthenticated } from "../middlewares/index";
 
 export default (router: express.Router) => {
-    router.post("/api/createappointment", isAuthenticated, createappointment);
-    //   router.get('/get-queue/specialization/:specialization', getBySpecialization);
-    //   router.get('/get-queue/doctor/:doctorId', getByDoctor);
-    //   router.get('/queue/:queueId', getQueueById); // GET request to retrieve queue by ID
+    router.post("/api/create-appointment", isAuthenticated, createAppointment);
+    router.get("/api/appointments/:queue_id", isAuthenticated, getAppointments);
+    router.put("/api/appointments/suspended/:appointment_id", isAuthenticated, markAppointmentAsSuspended);
+    // router.put("/api/appointments/operated/:appointment_id", isAuthenticated, markAppointmentAsOperated);
+    router.put("/api/appointments/completed/:appointment_id", isAuthenticated, markAppointmentAsCompleted);
+    router.get("/api/user/appointments/:status/:user_id", isAuthenticated, getAppointmentsByUserId);
 };
