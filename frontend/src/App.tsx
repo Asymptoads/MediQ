4ed482b5fd6532298359c3fc730947d1323189c7
Subekshyeah <Kadelsubekshya@gmail.com>
2025-01-10 19:06:25 +0545

    little bit of login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,8 @@ import { Box } from '@chakra-ui/react';
 
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-// import Register from './pages/Register/Register';
-// import MyAccount from './pages/MyAccount/MyAccount';
+import Register from './pages/Register/Register';
+import MyAccount from './pages/MyAccount/MyAccount';
 
 
 import './App.scss';
@@ -39,8 +39,8 @@ const App = () => {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
-                    {/* <Route path='/register' element={<Register />} />
-                    <Route path='/my-account' element={<MyAccount />} /> */}
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/my-account' element={<MyAccount />} />
                 </Routes>
             {/* </CalendarContextProvider> */}
         </Box>
